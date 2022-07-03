c0c225ea19d7d40fff7d9a76b18532dca74a532d
chico <chico.zimcolor@gmail.com>
2022-07-03 10:12:59 -0300

    using bigger CV images

--- src/enums/cvPathEnum.ts
+++ src/enums/cvPathEnum.ts
@@ -1,7 +1,6 @@
 enum CVPathEnum {
-  english = "https://docs.google.com/drawings/d/e/2PACX-1vR9Luxbg0r1QlsGrVEUIiiYT4JMlBfTS100_wC0ghh_OqNJSR2qToLzI-4Kiw0JcwQS2OKSaw9a6iyZ/pub?w=697&amp;h=1013",
-  brazilian = "https://docs.google.com/drawings/d/e/2PACX-1vQGHvijOg4RCtH-UbEh0rnxUoaEkvbjl3RaWqha6U2AX9L5CFOA-wEbp65U652S2gPohpPv6EiC9J8s/pub?w=697&amp;h=1013",
-  raw = "https://docs.google.com/drawings/d/e/2PACX-1vR9Luxbg0r1QlsGrVEUIiiYT4JMlBfTS100_wC0ghh_OqNJSR2qToLzI-4Kiw0JcwQS2OKSaw9a6iyZ/pub",
+  english = "https://docs.google.com/drawings/d/e/2PACX-1vR9Luxbg0r1QlsGrVEUIiiYT4JMlBfTS100_wC0ghh_OqNJSR2qToLzI-4Kiw0JcwQS2OKSaw9a6iyZ/pub?w=1046&h=1519",
+  brazilian = "https://docs.google.com/drawings/d/e/2PACX-1vQGHvijOg4RCtH-UbEh0rnxUoaEkvbjl3RaWqha6U2AX9L5CFOA-wEbp65U652S2gPohpPv6EiC9J8s/pub?w=1046&h=1519",
 }
 
 export default CVPathEnum;
